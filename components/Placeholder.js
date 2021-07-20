236b3479b9ffe53f45c0ff282707bc8245eaf3f1
ronan <ronan@thenextweb.com>
2021-07-20 16:25:16 +0200

    random comp

--- components/Placeholder.js
+++ components/Placeholder.js
@@ -1,10 +1,24 @@
+import { useEffect, useState } from 'react'; 
+
 const Placeholder = ({ text }) => {
+    const [ blerp, setBlerp ] = useState('Pffft')
+    
+    const Floodle = () => {
+        setTimeout(() => {
+            setBlerp('Gah!')
+        },3000)
+    };
+
+    useEffect(() => {
+        Floodle()
+    }, [])
 
     return(
         <div>
             <h3>All I do is hold this place</h3>
             <p>(placeholding intensifies)</p>
             <p>{text ? text : 'errf'}</p>
+            <h4>{blerp}</h4>
         </div>
     )
 
